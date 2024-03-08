b3209fda093d1c4512248c5610765cb11a44a2fa
“Dennis <dennisnzioki019@gmail.com>
2024-03-09 00:05:54 +0300

    Create Routes.jsx

---
+++ src/Routes.jsx
@@ -0,0 +1,25 @@
+import Home from "./routes/Home";
+import Profile from "./routes/Profile";
+import Offers from "./routes/Offers";
+import ForgotPass from "./routes/ForgotPass";
+import Signin from "./routes/Signin";
+import Signup from "./routes/Signup";
+import AddListing from "./routes/AddListing";
+import EditListing from "./routes/EditListing";
+import PrivateRoute from "./components/PrivateRoute";
+import SingleListing from "./routes/SingleListing";
+import Category from "./routes/Category";
+
+export {
+	Home,
+	Profile,
+	Offers,
+	ForgotPass,
+	Signin,
+	Signup,
+	PrivateRoute,
+	AddListing,
+	EditListing,
+	SingleListing,
+	Category,
+};
